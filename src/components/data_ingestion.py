e1a9d810ace3d093a9282f8b5fe9b4a8c826d01c
Shreyas Chigurupati <84034817+shreyas-chigurupati07@users.noreply.github.com>
2025-01-22 12:34:21 -0800

    data transformation

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -5,7 +5,8 @@ from src.logger import logging
 import pandas as pd
 from sklearn.model_selection import train_test_split
 from dataclasses import dataclass
-
+from src.components.data_transformation import DataTransformation
+from src.components.data_transformation import DataTransformationConfig
 
 @dataclass
 class DataIngestionConfig:
@@ -43,4 +44,7 @@ class DataIngestion:
         
 if __name__ == "__main__":
     di = DataIngestion()
-    di.initiate_data_ingestion()
+    train_data, test_data = di.initiate_data_ingestion()
+
+    data_transformation= DataTransformation()
+    data_transformation.initiate_data_transformation(train_data, test_data)
---
+++ src/utils.py
@@ -0,0 +1,20 @@
+import os
+import sys
+import numpy as np
+import pandas as pd
+import dill
+from src.exception import CustomException
+
+def save_object(file_path, obj):
+    '''
+    This function saves the object to the file path
+    '''
+    try:
+        dir_path = os.path.dirname(file_path)
+        os.makedirs(dir_path, exist_ok=True)
+
+        with open(file_path, 'wb') as file:
+            dill.dump(obj, file)
+
+    except Exception as e:
+        raise CustomException(e, sys)
